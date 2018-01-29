7e8068c745638b753352ecabeb2bb609402336c6
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2018-01-29 11:40:42 +0100

    :sparkles: Add Rust

--- Installation script/post_install.sh
+++ Installation script/post_install.sh
@@ -3,8 +3,8 @@
 ## The first : Installing Homebrew
 if test ! $(which brew)
 then
-	echo 'Installing Homebrew'
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo 'Installing Homebrew'
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew update
@@ -20,15 +20,15 @@ mas signin $COMPTE "$PASSWORD"
 
 # Function to simplify installations with mas (source : https://github.com/argon/mas/issues/41#issuecomment-245846651)
 function install () {
-	# Check if the App is already installed
-	mas list | grep -i "$1" > /dev/null
+    # Check if the App is already installed
+    mas list | grep -i "$1" > /dev/null
 
-	if [ "$?" == 0 ]; then
-		echo "==> $1 is already installed"
-	else
-		echo "==> Installing $1..."
-		mas search "$1" | { read app_ident app_name ; mas install $app_ident ; }
-	fi
+    if [ "$?" == 0 ]; then
+        echo "==> $1 is already installed"
+    else
+        echo "==> Installing $1..."
+        mas search "$1" | { read app_ident app_name ; mas install $app_ident ; }
+    fi
 }
 
 echo 'Installing Cask, to install other apps.'
@@ -65,7 +65,7 @@ brew cask install atom cyberduck github-desktop gitkraken intellij-idea
 brew cask install java julia mamp macdown provisionql qlcolorcode qlimagesize
 brew cask install qlmarkdown qlstephen sequel-pro
 brew install ant bash-completion clisp dnsmasq git-lfs go gradle lua mailhog
-brew install mysql nginx openssl pandoc php72 python3
+brew install mysql nginx openssl pandoc php72 python3 rust
 #TODO: ERROR for license
 go get -u github.com/nishanths/license
 license -update
